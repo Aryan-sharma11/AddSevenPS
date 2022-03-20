98adff396a006fd2ce58cab90eb845ee404630f4
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-03-21 00:31:12 +0530

    Source code for Add-Seven file

--- AddSevenPS/Library.fs
+++ AddSevenPS/Library.fs
@@ -1,4 +1,19 @@
 ﻿namespace AddSevenPS
 
-type Class1() = 
-    member this.X = "F#"
+open System.Management.Automation
+open System.Management.Automation.Runspaces
+// Cmdlet attribute declaration
+[<Cmdlet("Add", "Seven")>] 
+type GetNum() =
+     // Inheriting the PSCmdlet class
+    inherit PSCmdlet()
+    // Allows the cmdlet let to take default input
+    // Positional parameter to allow Cmdlet to take input without specifying the variable
+    [<Parameter(Position = 0)>] 
+    member val Num : string = "" with get , set
+    override this.EndProcessing() =
+        // Typecasting String to Integer
+        let num = this.Num |> int
+        // This is acting as Cmdlet class instance to access its methods 
+        this.WriteObject(7 + num)
+        base.EndProcessing()
