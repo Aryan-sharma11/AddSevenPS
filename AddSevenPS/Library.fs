04742e1d291aa8da8a49d6de9a4d34bd243e2fba
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-03-25 01:36:54 +0530

    Add Passing Tests

--- AddSevenPS/Library.fs
+++ AddSevenPS/Library.fs
@@ -6,6 +6,13 @@ open System.Management.Automation.Runspaces
 
 module AddSeven =
     let addSeven num = num + 7
+     
+    // For testing purpose
+    // Recieving a list of integers and then returning the list with solutions mapped to the list 
+    let add7 numberseq =
+        
+        // Mapping the solutions to the list of test cases
+        numberseq |> Seq.map addSeven
 
 
 // Cmdlet attribute declaration
