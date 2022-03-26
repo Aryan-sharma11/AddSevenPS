b96e2a87b512d4d9ed0aec642722ce2ffdd56239
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-03-26 15:22:08 +0530

    Added XUnit framework to test solution and updated test name

--- tests/LibraryTest.fs
+++ tests/LibraryTest.fs
@@ -1,33 +1,25 @@
-module tests
-
-open System.Management.Automation
+module AddSevenPSTest
 
 open System
+
+// Importing XUnit testing framework
+open Xunit
 
 // Importing our base namespace
 open AddSevenPS
 
-// Importing NUnit testing framework
-open NUnit.Framework
-
-// [<TestFixture>] marks the class that contains tests
-[<TestFixture>]
-
-type TestClass () =
-    
-    // Beginning of a test
-    [<Test>]
-    member this.TestEvenSequence () =
-        
-        // Expected results for our test cases
-        let expected = [ 9 ; 11 ; 13 ; 15 ; 17 ]
-        
-        // A list with integers for testing 
-        let actual = AddSeven.``Add seven to list`` [ 2 ; 4 ; 6 ; 8 ; 10 ]
-       
-        // Determining that the result sequence is equal to our expected results
-        Assert.That( actual , Is.EqualTo( expected ) )
-    
+ // Beginning of a test
+[<Fact>]
+let `` AddSeven.addSeven on list of integers`` () =
+     
+     // A list with integers for testing
+     let actual = AddSeven.``Add seven to list`` [ 9 ; 25 ; -2 ; 5 ; 11 ]
+     
+     // Expected results for our test cases
+     let expected = [ 16 ; 32 ; 5 ; 12 ; 18 ]
+     
+     // Comparing actual results with expected results
+     Assert.Equal< Collections.Generic.IEnumerable<int> >(actual , expected)      
         
                          
         
