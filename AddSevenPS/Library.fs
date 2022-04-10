be8657a3e77d4020df3515d2ddb3ae9f2bf052e4
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-04-10 12:11:12 +0530

    Added comments for TryParse method

--- AddSevenPS/Library.fs
+++ AddSevenPS/Library.fs
@@ -17,12 +17,17 @@ module AddSevenPS=
         [<Parameter(Position = 0)>]
         member val Num : string = "" with get , set
         override this.EndProcessing() =
-                 
+            
+            // Mutable variable created
             let mutable result = 0
             
-            if System.Int32.TryParse(this.Num, &result) then
-                this.WriteObject(AddSevenFS.addSeven (result))
+            // Checking whether the input is valid or not.
+            // Int32 represents a 32-bit signed integer.
+            // TryParse method converts the string representation of a number to its 32-bit signed integer equivalent.
+            if System.Int32.TryParse( this.Num, &result) then
+                this.WriteObject( AddSevenFS.addSeven (result) )
+            
             else
-               this.WriteObject("Please enter a valid integer.")
+               this.WriteObject( "Please enter a valid integer." )
                     
-            base.EndProcessing()
+            base.EndProcessing() 
