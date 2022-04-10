159fc238c5e73d248e24c237dc7d88c945fb59c4
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-04-10 13:27:09 +0530

    Added comments and command to save tests result

--- PesterTests/Run-Pester.ps1
+++ PesterTests/Run-Pester.ps1
@@ -1,1 +1,5 @@
-Invoke-Pester -Output Detailed *Tests*
+# This will run all the Pester tests in the directory
+Invoke-Pester -Output Detailed *Tests* 
+
+# Saving the Tests results in xml file
+Invoke-Pester -OutputFile result.xml 
